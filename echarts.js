51331de04338d995de8d1c107cc16c707f3a6646
TomBoutin <144112026+TomBoutin@users.noreply.github.com>
2023-12-18 17:06:19 +0100

    Add files via upload

--- echarts.js
+++ echarts.js
@@ -4,7 +4,7 @@ await M.init();
 
 
 var tab = [
-  "ADAM Fabrice", "ADAMCZYK Natacha", "AYMARD Adrien", "AYMARD Alain", "BABIN Valentin", "BERTHIER Hélène", "CHANTELOUP Amelin", "CHUPIN Suzanne", "CREDEVILLE Maxime", "CRESPIN Benoit", "DAL BELLO Marine", "DEMAISON Guillaume", "DUBREUIL Anne-Sophie", "DULAC Benoit", "FEYDI Philippe", "FIAMMETTI Deborah", "FLITTI Eric", "GERAUD Fabien", "GOUDARD Bérénice", "GRASSET Véronique", "GUEDIRA Réda", "JARDOU Thomas", "JAUFFRET Manon", "JOUY Maxime", "KABAB Simon", "LAFONT Mathieu", "LAVEFVE Valérie", "LASCAUD Raphaël", "LAZARE Jean-Cédric", "LE BAIL Emma", "LECOMTE Catherine", "LU Inès", "MARTY Thomas", "MINDOLLOT Rémi", "MORA Frédéric", "MOUTAT Audrey", "NENIN Cédric", "PAILLIER Stéphane", "PINAUD Anaïs", "PORRO Heinich", "PORTAL Nicolas", "SABOURIN Erwan", "SINCLAIR Diego", "SPRINGINSFELD Denis", "THARAUD Sébastien", "TZVETKOVA Maria", "TURBELIN Pierre", "VALETTE Sophie", "VEILLON Pascal"
+  "ADAM Fabrice", "ADAMCZYK Natacha", "AYMARD Adrien", "AYMARD Alain", "BABIN Valentin", "BERTHIER Hélène", "CHANTELOUP Amelin", "CHUPIN Suzanne", "CREDEVILLE Maxime", "CRESPIN Benoit", "DAL BELLO Marine", "DEMAISON Guillaume", "DUBREUIL Anne-Sophie", "DULAC Benoit", "FEYDI Philippe", "FIAMMETTI Deborah", "FLITTI Eric", "GERAUD Fabien", "GOUDARD Bérénice", "GRASSET Véronique", "GUEDIRA Réda", "JARDOU Thomas", "JAUFFRET Manon", "JOUY Maxime", "KABAB Simon", "LAFONT Mathieu", "LAVEFVE Valérie", "LASCAUD Raphaël", "LAZARE Jean-Cédric", "LE BAIL Emma", "LECOMTE Catherine", "LU Inès", "MARTY Thomas", "MONDOLLOT Rémi", "MORA Frédéric", "MOUTAT Audrey", "NENIN Cédric", "PAILLIER Stéphane", "PINAUD Anaïs", "PORRO Heinich", "PORTAL Nicolas", "SABOURIN Erwan", "SINCLAIR Diego", "SPRINGINSFELD Denis", "THARAUD Sébastien", "TZVETKOVA Maria", "TURBELIN Pierre", "VALETTE Sophie", "VEILLON Pascal"
 ];
 
 let allProf = {};
@@ -12,31 +12,7 @@ let allProf = {};
 const data = M.getEvents('mmi1').concat(M.getEvents('mmi2').concat(M.getEvents('mmi3')));
 
 
-for (let ev of tab) {
 
-  allProf[ev] = data.filter((event) => { return event.title.includes(ev) });
-  
-}
-
-for (let ev in allProf) {
-  let cm = 0;
-  let td = 0;
-  let tp = 0;
-  for (let i = 0; i < allProf[ev].length; i++) {
-    if (allProf[ev][i].title.includes("CM")) {
-      cm += allProf[ev][i].hours;
-    }
-    if (allProf[ev][i].title.includes("TD")) {
-      td += allProf[ev][i].hours;
-    }
-    if (allProf[ev][i].title.includes("TP")) {
-      tp += allProf[ev][i].hours;
-    }
-  }
-  allProf[ev] = [cm, td, tp];
-};
-
-console.log(allProf);
 
 import * as echarts from 'echarts';
 
@@ -111,5 +87,38 @@ option = {
     },
   ]
 };
+
+for (let ev of tab) {
+
+  allProf[ev] = data.filter((event) => { return event.title.includes(ev) });
+  
+}
+
+for (let ev in allProf) {
+  
+    let cm = 0;
+    let td = 0;
+    let tp = 0;
+  
+    for (let i = 0; i < allProf[ev].length; i++) {
+  
+      if (allProf[ev][i].title.includes("CM")) {
+       cm += allProf[ev][i].hours;
+      }
+      if (allProf[ev][i].title.includes("TD")) {
+        td += allProf[ev][i].hours;
+      }
+      if (allProf[ev][i].title.includes("TP")) {
+        tp += allProf[ev][i].hours;
+      }
+    }
+  
+    option.series[0].data.push(cm);
+    option.series[1].data.push(td);
+    option.series[2].data.push(tp);
+}
+
+console.log(allProf);
+
 
 option && myChart.setOption(option);
