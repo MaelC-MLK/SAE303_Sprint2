3046d8212edadffaa438958e79b1345908cf8355
TomBoutin <144112026+TomBoutin@users.noreply.github.com>
2023-12-19 22:25:45 +0100

    Add files via upload

--- sunburst-drink-charts.js
+++ sunburst-drink-charts.js
@@ -19,7 +19,7 @@ for (let ev of tab) {
   for (let i = 1; i <= 6; i++) {
     let semesterEvents = profEvents.filter((event) => { return event.semester.includes(i.toString()) });
 
-    allProf[ev][ "Semestre " + i ] = {};
+    allProf[ev]["Semestre " + i] = {};
 
     // Supposons que 'resources' est un tableau de tous les cours possibles
     let resources = [...new Set(semesterEvents.map(event => event.ressources))];
@@ -27,7 +27,7 @@ for (let ev of tab) {
     for (let resource of resources) {
       let resourceEvents = semesterEvents.filter((event) => { return event.ressources === resource });
 
-      allProf[ev][ "Semestre " + i ][resource] = {
+      allProf[ev]["Semestre " + i][resource] = {
         "CM": resourceEvents.filter((event) => { return event.type.includes("CM") }),
         "TD": resourceEvents.filter((event) => { return event.type.includes("TD") }),
         "TP": resourceEvents.filter((event) => { return event.type.includes("TP") })
@@ -36,122 +36,128 @@ for (let ev of tab) {
   }
 }
 
-
+let data = [];
 
 let selectName = document.getElementById("intervenant");
 
 selectName.addEventListener("change", function () {
 
-  selectName.value = this.value;
+  update(selectName.value);
+  myChart.setOption(option);
 
-  
+
 
 });
 
+function update(name) {
 
-var data = [];
+  data.length = 0;
 
-for (let prof in allProf) {
-  if (prof === selectName.value) {
-    let profData = {
-      name: prof,
-
-      children: []
-    };
-
-    for (let sem in allProf[prof]) {
-      let semData = {
-        name: sem,
-
+  for (let prof in allProf) {
+    if (prof === name) {
+      let profData = {
+        name: prof,
+        
         children: []
       };
 
-      for (let course in allProf[prof][sem]) {
-        let courseData = {
-          name: course,
+      for (let sem in allProf[prof]) {
+        let semData = {
+          name: sem,
 
           children: []
         };
 
-        let typeHours = {};
+        for (let course in allProf[prof][sem]) {
+          let courseData = {
+            name: course,
 
-        for (let type in allProf[prof][sem][course]) {
-          allProf[prof][sem][course][type].filter(event => {
-            if (!typeHours[event.type]) {
-              typeHours[event.type] = 0;
-            }
-
-            typeHours[event.type] += event.hours;
-          });
-        }
-
-        for (let type in typeHours) {
-          let typeData = {
-            name: type,
-            value: typeHours[type],
-
+            children: []
           };
 
-          courseData.children.push(typeData);
+          let typeHours = {};
+
+          for (let type in allProf[prof][sem][course]) {
+            allProf[prof][sem][course][type].filter(event => {
+              if (!typeHours[event.type]) {
+                typeHours[event.type] = 0;
+              }
+
+              typeHours[event.type] += event.hours;
+            });
+          }
+
+          for (let type in typeHours) {
+            let typeData = {
+              name: type,
+              value: typeHours[type],
+
+            };
+
+            courseData.children.push(typeData);
+          }
+
+          semData.children.push(courseData);
         }
 
-        semData.children.push(courseData);
+        profData.children.push(semData);
       }
 
-      profData.children.push(semData);
+      data.push(profData);
     }
-
-    data.push(profData);
   }
 }
 
-// var data = [
-//   {
-//     name: 'S1',
-//     itemStyle: {
-//       color: '#da0d68'
-//     },
-//     children: [
-//       {
-//         name: 'R1.12',
-//         value: 10,
-//         itemStyle: {
-//           color: '#975e6d'
-//         }
-//       },
-//       {
-//         name: 'R1.15',
-//         itemStyle: {
-//           color: '#e0719c'
-//         },
-//         children: [
-//           {
-//             name: 'CM',
-//             value: 1,
-//             itemStyle: {
-//               color: '#f99e1c'
-//             }
-//           },
-//           {
-//             name: 'TD',
-//             value: 0,
-//             itemStyle: {
-//               color: '#ef5a78'
-//             }
-//           },
-//           {
-//             name: 'TP',
-//             value: 8,
-//             itemStyle: {
-//               color: '#f7f1bd'
-//             }
-//           }
-//         ]
-//       }
-//     ]
-//   },
-    
-// ];
+update(selectName.value);
+
+
+var previousdata = [
+  {
+    name: 'S1',
+    itemStyle: {
+      color: '#da0d68'
+    },
+    children: [
+      {
+        name: 'R1.12',
+        value: 10,
+        itemStyle: {
+          color: '#975e6d'
+        }
+      },
+      {
+        name: 'R1.15',
+        itemStyle: {
+          color: '#e0719c'
+        },
+        children: [
+          {
+            name: 'CM',
+            value: 1,
+            itemStyle: {
+              color: '#f99e1c'
+            }
+          },
+          {
+            name: 'TD',
+            value: 0,
+            itemStyle: {
+              color: '#ef5a78'
+            }
+          },
+          {
+            name: 'TP',
+            value: 8,
+            itemStyle: {
+              color: '#f7f1bd'
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+];
 
 
 
@@ -202,13 +208,5 @@ option = {
     ]
   }
 };
-
-
-
-
-
-
-
-
 
 option && myChart.setOption(option);
