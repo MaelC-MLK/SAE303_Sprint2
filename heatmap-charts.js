d2c6085c5ab38b573731500c68daf8066fc53706
Mael C <74120418+MaelC-MLK@users.noreply.github.com>
2023-12-20 14:39:52 +0100

    Add files via upload

--- heatmap-charts.js
+++ heatmap-charts.js
@@ -14,6 +14,7 @@ const dataAll = M.getEvents('mmi1').concat(M.getEvents('mmi2').concat(M.getEvent
 var chartDom = document.getElementById('heatmap-charts');
 var myChart = echarts.init(chartDom, "dark");
 var option;
+
 console.log(dataAll);
 
 // prettier-ignore
@@ -21,7 +22,7 @@ const hours = ["20h","19h","18h","17h","16h","15h","14h","13h","12h","11h","10h"
 // prettier-ignore
 const days = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi"];
 // prettier-ignore
-const data = [[0,0, 5]]
+const data = []
     .map(function (item) {
     return [item[1], item[0], item[2] || '-'];
 });
@@ -72,5 +73,48 @@ option = {
     }
   ]
 };
+
+
+// hour : un entier entre 0 et 23 qui détermine une tranche d'une heure (ex: 14 pour la tranche 14h-15h)
+// start : un objet Date qui détermine l'heure de début d'un cours
+// end : un objet Date qui détermine l'heure de fin d'un cours
+// retourne la durée de l'intersection entre [start, end] et [hour, hour+1] en heures (0 si pas d'intersection)
+
+let intersectByHour = function (hour, start, end){
+
+  let interStart = new Date(start);
+  interStart.setHours(hour);
+  interStart.setMinutes(0);
+  let interEnd = new Date(end);
+  interEnd.setHours(hour+1);
+  interEnd.setMinutes(0);
+  
+  // maintenant il faut déterminer s'il existe une intersection entre [interStart, interEnd] et les horaires du cours [start, end]
+  
+  if (interEnd<=start) // si l'heure de fin est avant l'heure de début du cours, pas d'intersection
+  return 0;
+  else if (interStart>=end) // si l'heure de début est après l'heure de fin du cours, pas d'intersection
+  return 0;
+  else{ // il existe une intersection entre [interStart, interEnd] et les horaires du cours [start, end]
+  return ( Math.min(interEnd, end) - Math.max(interStart, start) ) / 1000 / 3600 ;
+  // on prend le minimum des deux heures de fin et le maximum des deux heures de début
+  // et on retourne la durée de l'intersection en heures
+  }  
+  }
+
+
+  console.log(dataAll[0].end);
+
+  console.log(intersectByHour(10, dataAll[0].start, dataAll[0].end));
+
+  for(let i=0; i<days.length; i++){
+    for(let j=0; j<hours.length; j++){
+      let sum = 0;
+      for(let k=0; k<dataAll.length; k++){
+        sum += intersectByHour(j+8, dataAll[k].start, dataAll[k].end);
+      }
+      data.push([j+8, hours[j], sum]);
+    }
+  }
 
 option && myChart.setOption(option);
